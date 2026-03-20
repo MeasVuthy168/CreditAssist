5473e6f8eb06eacf4bddd4b4fe906507906ffa1d
MeasVuthy168 <measvuthy21@gmail.com>
2026-03-20 21:13:26 +0700

    Fix syntax error in config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// config.js
 window.CONFIG = {
-    BASE_URL: "https://secure-backend-new.onrender.com"
-    //BASE_URL: "https://secure-backend-tzj9.onrender.com"
+  BASE_URL: "https://secure-backend-new.onrender.com"
 };
